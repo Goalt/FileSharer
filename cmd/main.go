9a4781e3916c67b2e52fd465903a9aa9e4fd7bee
Yuri Konkov <konkov96@gmail.com>
2021-07-31 02:21:32 +0300

    dev ready

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
+
 	"github.com/Goalt/FileSharer/cmd/subcomands"
 	_ "github.com/Goalt/FileSharer/cmd/subcomands/file_sharer_migrations"
 	"github.com/Goalt/FileSharer/internal/config"
 	"github.com/Goalt/FileSharer/internal/provider"
 	"github.com/sethvargo/go-signalcontext"
 	"github.com/urfave/cli/v2"
-	"os"
 )
 
 var (
@@ -31,6 +32,9 @@ func main() {
 		Action: func(ctx *cli.Context) error {
 			cfg := config.Config{
 				DebugLevel: ctx.Int(DebugLevel),
+				Server: config.Server{
+					Port: 8080,
+				},
 			}
 
 			signalCtx, cancel := signalcontext.OnInterrupt()
